2311824c967a19ffb545d41941f33aa804fd0f64
Matteo <matteo.fereire@gmail.com>
2020-11-09 15:22:43 +0100

    ajout des images et des catalogues fruits et legumes

---
+++ Data/CatalogueFruit.js
@@ -0,0 +1,62 @@
+var cataloguefruit = [
+    {
+        "name": "Pomme",
+        "description": "une bonne pomme bien mûre",
+        "Image":"Images/pomme.jpg",
+        "price":"1"
+    },
+    {
+        "name": "Poire",
+        "description": "une bonne poire bien juteuse",
+        "Image":"Images/poire.jpg",
+        "price":"1.10"
+    },
+    {
+        "name": "Banane",
+        "description": "une joli banane",
+        "Image":"Images/banane.jpg",
+        "price":"1.05"
+    },
+    {
+        "name": "Ananas",
+        "description": "un splendide ananas",
+        "Image":"Images/ananas.jpg",
+        "price":"2"
+    },
+    {
+        "name": "Pêche",
+        "description": "une pêche toute douce",
+        "Image":"Images/pêche.jpg",
+        "price":"0.80"
+    },
+    {
+        "name": "Clémentine",
+        "description": "une clémentine venant de Corse",
+        "Image":"Images/clémentine.jpg",
+        "price":"0.50"
+    },
+    {
+        "name": "Raisin",
+        "description": "Des raisins bien mûre",
+        "Image":"Images/raisin.jpg",
+        "price":"1.60"
+    },
+    {
+        "name": "Mangue",
+        "description": "une mangue bien mûre",
+        "Image":"Images/mangue.jpg",
+        "price":"2.20"
+    },
+    {
+        "name": "Pastèque",
+        "description": "une bonne grosse pastèque",
+        "Image":"Images/pasteque.jpg",
+        "price":"3"
+    },
+    {
+        "name": "Cerise",
+        "description": "Des cerises bien rouges",
+        "Image":"Images/cerise.jpg",
+        "price":"1.30"
+    }
+]
---
+++ Data/CatalogueLegume.js
@@ -0,0 +1,62 @@
+var cataloglegume = [
+    {
+    "name": "Courgette",
+    "description": "Une courgette bio bien verte",
+    "Image": "Images/courgette.jpg",
+    "price": "3"
+    },
+    {
+        "name": "Aubergine",
+        "description": "Une aubergine violette",
+        "Image": "Images/courgette.jpg",
+        "price": "4"
+    },
+    {
+        "name": "Pomme de la terre",
+        "description": "Patate, pomme de terre ou patate de la terre pour certains, elles restent succulentes",
+        "Image": "Images/patate.jpg",
+        "price": "2"
+    },
+    {
+        "name": "Carotte",
+        "description": "Les lapins ne seront pas les seuls à les aimer",
+        "Image": "Images/carotte.jpg",
+        "price": "2.5"
+    },
+    {
+        "name": "Salade verte",
+        "description": "Tendre salade verte fraichement cultivée",
+        "Image": "Images/salade-verte.jpg",
+        "price": "1.5"
+    },
+    {
+        "name": "Haricots",
+        "description": "De bons haricots verts blanchis",
+        "Image": "Images/haricots-verts.jpg",
+        "price": "4"
+    },
+    {
+        "name": "Artichaut",
+        "description": "Artichaut a manger bien chaud",
+        "Image": "Images/artichaut.jpg",
+        "price": "4"
+    },
+    {
+        "name": "Navet",
+        "description": "Navet rosé et BIO",
+        "Image": "Images/navet.jpg",
+        "price": "3.5"
+    },
+    {
+        "name": "Endive",
+        "description": "Mais qui aime donc les endives? ",
+        "Image": "Images/endive.jpg",
+        "price": "0.25"
+    },
+    {
+        "name": "Radis",
+        "description": "Bon petits radis qui ne piquent pas",
+        "Image": "Images/radis.jpg",
+        "price": "2.25"
+    }
+]
